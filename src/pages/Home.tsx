ae93df8f3996529feed7948384a9fbf33726b5de
Tiago <tiagodev@protonmail.com>
2024-02-06 18:18:06 -0300

    feat: styling the layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,3 +5,39 @@ export default function Home() {
     </div>
   )
 }
+
+/**
+ * import { motion } from "framer-motion";
+import './App.css'
+
+function TextAnimation() {
+  const text = 'Hello word'
+  return (
+    <motion.div className="text">
+      {text.split('').map((letter, index) => (
+        <motion.span
+          key={index}
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{ duration: 1, delay: index * 0.1}}
+          >
+            {letter}
+          </motion.span>
+      ))}
+    </motion.div>
+  )
+}
+
+
+
+function App() {
+
+  return (
+    <>
+      <TextAnimation />
+    </>
+  )
+}
+
+export default App
+ */
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,18 +1,22 @@
 import { Outlet, Link } from 'react-router-dom';
+import picture from '../assets/profile-pic.png'
 
 export default function Layout() {
   return (
     <>
-      <div className="container flex mx-auto px-4 bg-cyan-600 text-white">
-        <nav className="flex flex-col items-center justify-between p-4">
-          <h1 className="text-4xl font-bold">My Portfolio</h1>
-          <div className="flex flex-col text-2xl">
+      <div className="container min-h-screen flex mx-auto bg-cyan-600 text-white font-mono">
+        <nav className="flex flex-col items-center justify-around min-h-screen box-border border-r border-white w-80 p-4">
+          <div className='flex items-center justify-center rounded-full bg-slate-100 h-60 w-60'>
+            <img src={picture} alt="profile" className="object-cover h-56 w-56" />
+          </div>
+          <div className="flex flex-col w-full justify-evenly items-center text-3xl font-semibold h-96">
             <Link to="/">Home</Link>
             <Link to="about">About</Link>
             <Link to="resume">Resume</Link>
             <Link to="projects">Projects</Link>
             <Link to="contact">Contact</Link>
           </div>
+          <footer className="w-full text-center text-white font-mono font-semibold"><p>@2024 <b className='text-white font-mono font-semibold'>Portfolio - All rights reserved</b></p></footer>
         </nav>
 
         <div className="p-4">
