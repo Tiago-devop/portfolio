cc551bc5e25cad4494a65a1fdab251537cd8428c
Tiago <tiagodev@protonmail.com>
2024-02-27 12:52:23 -0300

    Update About page with services section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="w-full h-full">
-      <h1 className="text-7xl">ABOUT ME</h1>
+      <h2 className="text-7xl">ABOUT ME</h2>
       <div>
         <div className="w-[500px] h-[500px] bg-slate-400 flex justify-center items-center">
           <p className="text-white text-6xl">IMG</p>
@@ -15,6 +15,25 @@ export default function About() {
           <a href="https://drive.google.com/uc?export=download&id=1lZwbSv6A6KLb2_24LfTPMTxf_mFAN73j" target="_blank" rel="noopener noreferrer" className="bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded">
             Download PDF
           </a>
+        </div>
+      </div>
+      <h2 className="text-7xl">SERVICES</h2>
+      <div className="flex w-full justify-evenly">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><div className="w-full h-72 bg-slate-400 text-6xl text-white flex justify-center items-center">IMG</div></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><div className="w-full h-72 bg-slate-400 text-6xl text-white flex justify-center items-center">IMG</div></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            
+          </div>
         </div>
       </div>
     </div>
