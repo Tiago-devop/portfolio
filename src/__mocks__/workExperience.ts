5142ccbbb4ceb62623d3a446e5ec88347bcb820b
Tiago <tiagodev@protonmail.com>
2024-12-05 22:08:37 -0300

    chore: Upgrade React and React-DOM to version 19.0.0; update work experience mock data with new entries and details

--- src/__mocks__/workExperience.ts
+++ src/__mocks__/workExperience.ts
@@ -1,23 +1,73 @@
 export const workExperience = [
   {
-    company: "Company 1",
-    position: "Frontend Developer",
-    startDate: "2020",
-    endDate: "2021",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet est nec elit gravida tincidunt. Ut at odio nec elit gravida tincidunt."
+    company: "Santuário da medalha milagrosa",
+    position: "Volunteer Frontend Developer",
+    startDate: "2024/Jan",
+    endDate: "",
+    bulletPoints: ["Application Development: Created a news management platform for PASCOM (Pastoral Communication) using React for the frontend and Strapi as the backend, facilitating the administration and dissemination of parish information.",
+      "Team Collaboration: Worked closely with the pastoral team, conducting meetings to understand the specific needs of the parish, ensuring that the solution met user expectations and requirements.", "Design and Prototyping: Utilized Figma to develop interactive prototypes, allowing for a clear visualization of user flow and interface before development.", "Volunteer Work: Contributed to the project as part of a volunteer effort, dedicating time and skills to strengthen communication and evangelization within the local community.", "Feature Implementation: Integrated features that enable the publication and editing of news articles, as well as optimized the layout for an improved user experience."
+    ],
+    skillAcquired: ["Development in React and Strapi", "Prototyping with figma", "Effective communication with stakeholders", "Management of volunteer projects"],
+    description: "This experience not only enhanced my technical skills but also reinforced my commitment to community service and the importance of collaborative work."
   },
   {
-    company: "Company 2",
-    position: "Web Designer",
-    startDate: "2018",
-    endDate: "2020",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet est nec elit gravida tincidunt. Ut at odio nec elit gravida tincidunt."
+    company: "SoulCode Academy",
+    position: "Salesforce Developer",
+    startDate: "2022/Jul",
+    endDate: "2022/Dec",
+    bulletPoints: [
+      "Salesforce Development: Developed applications using Salesforce technologies including LWC, Aura, and Visualforce for the frontend, and SOQL, Apex, Test Classes, and Triggers for the backend.",
+      "Salesforce Administration: Managed Salesforce administration tasks such as creating Validation Rules, utilizing Process Builder, and implementing security measures to ensure data integrity.",
+      "Collaboration: Worked in a team environment to deliver high-quality solutions while adhering to best practices in Salesforce development.",
+      "Problem Solving: Engaged in troubleshooting and debugging processes to resolve issues efficiently within the Salesforce platform.",
+      "Continuous Learning: Participated in training sessions and workshops to stay updated with the latest Salesforce features and enhancements."
+    ],
+    skillAcquired: [
+      "Salesforce Lightning Development",
+      "Apex Programming",
+      "SOQL Querying",
+      "Salesforce Administration Best Practices"
+    ],
+    description: "This experience strengthened my technical skills in Salesforce development while enhancing my ability to work collaboratively in a dynamic environment."
   },
   {
-    company: "Company 3",
+    company: "Etho Soluções",
     position: "Frontend Developer",
-    startDate: "2016",
-    endDate: "2018",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet est nec elit gravida tincidunt. Ut at odio nec elit gravida tincidunt."
+    startDate: "2022/Jun",
+    endDate: "2022/Jul",
+    bulletPoints: [
+      "Frontend Development: Built user interfaces using ReactJS, incorporating Styled-components, React Router v6, React-Redux v7, and Material-UI for enhanced styling and functionality.",
+      "Backend Integration: Developed backend functionalities using Node.js, Express, and Mongoose, ensuring seamless data handling and authentication with JWT.",
+      "Performance Optimization: Focused on code readability and performance enhancements to create efficient and maintainable codebases.",
+      "Version Control: Utilized GIT for versioning and collaboration on projects, ensuring organized code management and team collaboration.",
+      "Agile Methodologies: Engaged in agile practices to streamline development processes and improve team productivity."
+    ],
+    skillAcquired: [
+      "ReactJS Development",
+      "TypeScript Proficiency",
+      "Node.js Backend Development",
+      "Understanding of Agile Methodologies"
+    ],
+    description: "This experience provided a solid foundation in frontend development while enhancing my skills in collaborative coding and agile project management."
+  },
+  {
+    company: "Damiens & Devs",
+    position: "Junior Frontend Developer",
+    startDate: "2021/Sep",
+    endDate: "2022/Jul",
+    bulletPoints: [
+      "Collaboration in Systems Development: Contributed to the development of applications and program design using TypeScript, JavaScript, Styled-components, HTML5, and SCSS.",
+      "Agile Methodologies: Engaged in Agile practices, including Scrum and Kanban, utilizing Atlassian Jira for effective project management during Sprints.",
+      "Code Quality: Focused on writing clean and maintainable code, ensuring high standards of performance and readability.",
+      "Team Collaboration: Worked closely with other developers to enhance application features and resolve issues collaboratively.",
+      "Continuous Improvement: Participated in code reviews and feedback sessions to improve both personal and team coding practices."
+    ],
+    skillAcquired: [
+      "Proficiency in TypeScript and JavaScript",
+      "Experience with Styled-components and SCSS",
+      "Understanding of Agile methodologies",
+      "Familiarity with project management tools like Atlassian Jira"
+    ],
+    description: "This freelance experience allowed me to develop my technical skills in frontend development while collaborating effectively within a team-oriented environment."
   }
 ]
